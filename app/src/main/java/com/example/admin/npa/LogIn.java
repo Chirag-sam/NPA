0928af3f03cb5d367dcbbc27fab2ca3d3c72e834
Chirag-sam <chiraggsam@gmail.com>
2017-03-03 15:36:37 +0530

    mainactivity

--- app/src/main/java/com/example/admin/npa/LogIn.java
+++ app/src/main/java/com/example/admin/npa/LogIn.java
@@ -1,13 +1,17 @@
 package com.example.admin.npa;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LogIn extends AppCompatActivity {
 
@@ -21,11 +25,19 @@ public class LogIn extends AppCompatActivity {
     TextInputLayout edittextdialtil1;
     @BindView(R.id.cv)
     CardView cv;
+    @BindView(R.id.butt)
+    Button butt;
+    @OnClick(R.id.butt)
+    void submit()
+    {
+        startActivity(new Intent(LogIn.this,MainActivity.class));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
         ButterKnife.bind(this);
+
     }
 }
